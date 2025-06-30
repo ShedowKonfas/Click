7a9ce87e2970e9f612c1755b5c0c70c7ba67660d
ShedowKonfas <128817836+ShedowKonfas@users.noreply.github.com>
2025-07-01 01:09:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import telebot
 import pyautogui
 import time
+import random
 
 text_chest = False
+hotkey_chest = False
+
+hype_rand = ["HYPE!!!-Legendary", "hype-epic", "hype-rare"]
+probabilities = [0.05, 0.3, 0.65]  # Сумма 1 (5%, 30%, 65%)
+result = random.choices(hype_rand, weights=probabilities, k=1)[0]
+
 
 # Инициализация бота с токеном
 bot = telebot.TeleBot("7501578133:AAGQPP1qEpBusawjZ7xt6CAXqd51a1EOdHs")
@@ -23,37 +30,54 @@ def click_mouse(message):
     bot.reply_to(message, "Начинаю имитировать нажатия мыши...")
     for _ in range(1):  # Количество нажатий
         pyautogui.click()  # Имитируем нажатие левой кнопки мыши
-        #time.sleep(1)  # Задержка между нажатиями (1 секунда)
+        # time.sleep(1)  # Задержка между нажатиями (1 секунда)
     bot.reply_to(message, "Имитирование нажатий завершено.")
 
 # Обработчик команды /shutdown
-#@bot.message_handler(commands=['shutdownn'])
-#def press_hotkeys(message):
-    #pyautogui.hotkey('win','r')
-    #pyautogui.write("shutdown /s /t 2")
-    #pyautogui.press("Enter")
+@bot.message_handler(commands=['shutdown', "sh"])
+def shutdown(message):
+    pyautogui.hotkey('win', 'r')
+    pyautogui.write("shutdown /s /t 120")
+    pyautogui.press("Enter")
 
 # Обработчик команды /press
-#@bot.message_handler(commands=['press'])
-#def press_hotkeys(message):
-    #pyautogui.hotkey('win','r')
-    #pyautogui.write("shutdown /s /t 2000")
-    #pyautogui.press("Enter")
+@bot.message_handler(commands=['press'])
+def press_hotkeys(message):
+    global hotkey_chest
+    hotkey_chest = True
+    bot.reply_to(message, "Какую комбинацию клавиш нажать? Пример: win r")
 
 # Обработчик команды /type
 @bot.message_handler(commands=['type'])
-def press_message(message):
+def type_message(message):
     global text_chest
     text_chest = True
-    bot.reply_to(message, "Какую фразу написать???")
+    bot.reply_to(message, "Какую фразу написать?")
 
+# Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
-def type_text(message):
-    global text_chest
-    text_to_type=message.text
-    time.sleep(2)
-    pyautogui.write(text_to_type)
-    text_chest = False
+def handle_text(message):
+    global text_chest, hotkey_chest
+
+    if hotkey_chest:
+        hotkey_to_press = message.text.split()  # Разделяем строку на отдельные клавиши
+        time.sleep(2)
+        pyautogui.hotkey(*hotkey_to_press)  # Используем распаковку аргументов
+        hotkey_chest = False
+        bot.reply_to(message, "Комбинация клавиш нажата.")
+
+    elif text_chest:
+        text_to_type = message.text
+        time.sleep(2)
+        pyautogui.write(text_to_type)
+        text_chest = False
+        bot.reply_to(message, "Фраза написана.")
+
+
+# Обработчик стикеров
+@bot.message_handler(content_types=['sticker'])
+def handle_sticker(message):
+    bot.reply_to(message, result)
 
 # Запуск бота
 bot.polling()
